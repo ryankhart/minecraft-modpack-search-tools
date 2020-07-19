e5f824ad97bca2bd49eb237410dd8506707412ae
Ryan Hart <3673962+ryankhart@users.noreply.github.com>
2020-07-18 19:02:36 -0700

    combined into 1 function, no reason to do one and not other

--- script.py
+++ script.py
@@ -2,22 +2,34 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 
 
-def get_urls_in_current_page(page_url):
-    driver.get(page_url)
-    driver.implicitly_wait(WAIT)
-    sub_soup = BeautifulSoup(driver.page_source, 'html.parser')
+def get_modpack_urls_list(url):
+    driver.get(url)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    pagination_items = soup.find_all('a', class_='pagination-item')
+    num_pages = pagination_items[len(pagination_items)-1].text
+    
+    retVal = []
+
+    for page_num in range(1, 2):  # int(num_pages)):
+        new_page_url = DEPENDENTS_URL + '?page=' + str(page_num)
+        # get_urls_in_current_page(new_page_url)
+        
+        driver.get(new_page_url)
+        sub_soup = BeautifulSoup(driver.page_source, 'html.parser')
 
     modpack_listing_rows = sub_soup.find_all(
         'li', class_='project-listing-row box py-3 px-4 flex flex-col lg:flex-row lg:items-center mb-2')
 
-    for listing_row in range(0, len(modpack_listing_rows)):
-        modpack_urls = modpack_listing_rows[listing_row].find_all('a')
-        MODPACK_ICON = 0
-        MODPACK_NAME = 1
-        MODPACK_AUTHOR = 2
-        # 3+ = categories
-        url = 'https://www.curseforge.com' + modpack_urls[MODPACK_NAME]['href']
-        modpack_urls_all.append(url)
+        for listing_row in range(0, len(modpack_listing_rows)):
+            modpack_urls = modpack_listing_rows[listing_row].find_all('a')
+            MODPACK_ICON = 0
+            MODPACK_NAME = 1
+            MODPACK_AUTHOR = 2
+            # 3+ = categories
+            url = 'https://www.curseforge.com' + modpack_urls[MODPACK_NAME]['href']
+            retVal.append(url)
+    
+    return retVal
 
 
 def scrape_modpack_pages(urls):
