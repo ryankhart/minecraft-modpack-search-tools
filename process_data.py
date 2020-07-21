460c6bcd5e5372188dd36a8bc81d1c397b39b484
Ryan Hart <3673962+ryankhart@users.noreply.github.com>
2020-07-20 21:25:36 -0700

    finished sort

--- process_data.py
+++ process_data.py
@@ -5,8 +5,8 @@ def filter_by_game_version(data, game_version_list):
     return [d for d in data if any(item in d['game_versions'] for item in game_version_list)]
 
 
-def sort_by(data):
-    pass
+def sort_by(data, key, should_reverse=False):
+    return sorted(data, key=lambda i: i[key], reverse=should_reverse)
 
 
 def print_list(data):
@@ -18,5 +18,5 @@ if __name__ == "__main__":
     with open('data.json', "r") as data_file:
         data = json.load(data_file)['modpack_metadata']
     filtered_data = filter_by_game_version(data, ['1.12.2'])
-    print(filtered_data)
-    
+    sorted_data = sort_by(filtered_data, 'total_downloads', True)
+    print(sorted_data)
