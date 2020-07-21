8bb3ed432e0ebb0c411c7628558469f853f15585
Ryan Hart <3673962+ryankhart@users.noreply.github.com>
2020-07-20 22:13:21 -0700

    finished print_list

--- process_data.py
+++ process_data.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 
 def filter_by_game_version(data, game_version_list):
@@ -6,11 +7,15 @@ def filter_by_game_version(data, game_version_list):
 
 
 def sort_by_key(data, key, should_reverse=False):
-    return sorted(data, key=lambda i: i[key], reverse=should_reverse)
+    return sorted(data, key=lambda i: int(i[key].replace(',', '')), reverse=should_reverse)
 
 
-def print_list(data):
-    pass
+def print_list(data, exclude_keys=[]):
+    for d in data:
+        print('Project Name: ' + d['project_name'])
+        print('Total Downloads: ' + d['total_downloads'])
+        print('Last Updated: ' + time.strftime('%Y-%m-%d', time.localtime(int(d['date_updated']))))
+        print()
 
 
 if __name__ == "__main__":
@@ -19,4 +24,4 @@ if __name__ == "__main__":
         data = json.load(data_file)['modpack_metadata']
     filtered_data = filter_by_game_version(data, ['1.12.2'])
     sorted_data = sort_by_key(filtered_data, 'total_downloads', True)
-    print(sorted_data)
+    print_list(sorted_data)
